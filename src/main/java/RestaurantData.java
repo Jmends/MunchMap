1b7385622c654667fba8b08ced6e5e764de8374f
Jmends <jonathanamends7@gmail.com>
2024-11-16 21:47:48 -0600

    added api key to environment variable and started cords method

--- src/main/java/RestaurantData.java
+++ src/main/java/RestaurantData.java
@@ -1,3 +1,15 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class RestaurantData {
+    //api key
+    private static final String API_KEY = System.getenv("GOOGLE_API");
+
+    public static double[] getCordinates(String address, String api){
+
+        return new double[] {1,2}; //place holder
+    }
 
 }
