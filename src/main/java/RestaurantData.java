2f892cbe308eb124ed112285bf456a2a4d14ebe7
Jmends <jonathanamends7@gmail.com>
2024-11-30 02:50:13 -0600

    added api url and the address to get correct json data

--- src/main/java/RestaurantData.java
+++ src/main/java/RestaurantData.java
@@ -1,15 +1,27 @@
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+import org.json.JSONArray;
 
 public class RestaurantData {
     //api key
     private static final String API_KEY = System.getenv("GOOGLE_API");
 
-    public static double[] getCordinates(String address, String api){
+    public static double[] getCordinates(String address, String api) {
+        try {
 
-        return new double[] {1,2}; //place holder
+            String encodedAddress = address.replace(" ", "+");
+            String urlString = "https://maps.googleapis.com/maps/api/geocode/json?address=" + encodedAddress + "&key=" + API_KEY;
+
+            URL url = new URL(urlString);
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+        return new double[]{1, 2}; //place holder
     }
 
 }
