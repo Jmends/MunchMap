8987281b52d11452fa129b39be7532a08d1aa8ac
Jmends <jonathanamends7@gmail.com>
2024-12-08 00:13:26 -0600

    added some code to test the methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,22 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+//        Scanner scan = new Scanner(System.in);
+//
+//        double coords[] = RestaurantData.getCoordinates("13411 Lake willoughby Ln");
+//
+//        int radius = RestaurantData.convertToMeters(5);
+//
+//        String food = RestaurantData.getRetsaurant(coords,radius);
+//
+//
+//        System.out.println(coords[0]);
+//        System.out.println(coords[1]);
+//
+//        System.out.println(radius);
+//        System.out.println(food);
+
+
 
     }
 }
